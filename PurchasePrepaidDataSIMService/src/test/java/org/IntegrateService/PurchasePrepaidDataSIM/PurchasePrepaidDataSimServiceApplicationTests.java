74b2126bc579224b7fd3fea1c6105ff89fd15212
vudq291295 <quangvu291295@gmail.com>
2021-03-27 16:11:39 +0700

    init prj

---
+++ PurchasePrepaidDataSIMService/src/test/java/org/IntegrateService/PurchasePrepaidDataSIM/PurchasePrepaidDataSimServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.IntegrateService.PurchasePrepaidDataSIM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasePrepaidDataSimServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
